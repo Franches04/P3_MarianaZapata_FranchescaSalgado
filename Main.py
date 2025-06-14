b2f9beca3b4d4934b4ec92a7841bd8d1502fc5a7
Mariana-Zapata-H <mariana.zapatah@udea.edu.co>
2025-06-14 17:24:57 -0500

    Opinión d

--- Main.py
+++ Main.py
@@ -51,6 +51,57 @@ def menu():
                 plt.show()
             except Exception as e:
                 print(f"Sucedió un error al cargar la imagen: {e}")
+                
+        elif op == 'd':
+            print("\n Trasladar imagen DICOM cargada")
+            clave = input("Clave DICOM: ").strip()
+            if clave in archivos_dicom:
+                handler = archivos_dicom[clave]
+                try:
+                    index = input("Índice del corte (Enter para usar 0): ")
+                    index = int(index) if index.strip() != "" else 0
+
+                    print("\nSelecciona una de las siguientes opciones para la dirección de traslación:")
+                    print("1) dx=50, dy=0 (Se va a mover a la derecha)")
+                    print("2) dx=-50, dy=0 (Se va a mover a la izquierda)")
+                    print("3) dx=0, dy=-130 (Va a moverse hacia arriba)")
+                    print("4) dx=0, dy=100 (Se moverá hacia abajo)")
+
+                    opcion = input("¿Qué opción desea de 1 a 4?: ").strip()
+
+                    if opcion == '1':
+                        dx, dy = 50, 0
+                    elif opcion == '2':
+                        dx, dy = -50, 0
+                    elif opcion == '3':
+                        dx, dy = 0, -130
+                    elif opcion == '4':
+                        dx, dy = 0, 100
+                    else:
+                        print("La opción es inválida. Se usará dx=10 y dy=10 por defecto.")
+                        dx, dy = 10, 10
+
+                    original, trasladada = handler.trasladar_imagen(index=index, dx=dx, dy=dy)
+
+                    fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+                    axs[0].imshow(original, cmap='gray')
+                    axs[0].set_title("Original")
+                    axs[1].imshow(trasladada, cmap='gray')
+                    axs[1].set_title(f"Imagen Trasladada (dx={dx}, dy={dy})")
+                    for ax in axs:
+                        ax.axis('off')
+                    plt.tight_layout()
+                    plt.show()
+
+                    nombre_archivo = f"trasladada_{clave}_dx{dx}_dy{dy}_index{index}.png"
+                    cv2.imwrite(nombre_archivo, trasladada)
+                    print(f"La imagen trasladada fue guardada como {nombre_archivo}")
+
+                except Exception as e:
+                    print(f"Hubo un error al trasladar la imagen: {e}")
+            else:
+                print("La clave no fue encontrada.")
+        
         
 
         
