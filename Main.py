6ebaaa8fb90bea033048a4bb18ba29ca2954adf6
Mariana-Zapata-H <mariana.zapatah@udea.edu.co>
2025-06-14 17:22:24 -0500

    Opción b

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ archivos_imagenes = {}
 
 def menu():
     while True:
-        print("\nMenú Principal \n a. Cargar carpeta DICOM \n b) Crear paciente \n c) Cargar imagen JPG/PNG \n d) Trasladar imagen DICOM" \
+        print("\nMenú Principal \n a). Cargar carpeta DICOM \n b) Crear paciente \n c) Cargar imagen JPG/PNG \n d) Trasladar imagen DICOM" \
         "\n e) Procesar imagen JPG/PNG \n f) Salir")
         op = input("Opción: ").lower() 
         
@@ -21,6 +21,20 @@ def menu():
                 gestor.mostrar_cortes()
                 archivos_dicom[clave] = gestor
             except Exception as e:
-                print(f"Se presentó un error al intentar cargar DICOM: {e}"
+                print(f"Se presentó un error al intentar cargar DICOM: {e}")
+                
+        elif op == 'b':
+            clave = input("Clave del DICOM cargado: ").strip()
+            if clave in archivos_dicom:
+                try:
+                    gestor = archivos_dicom[clave]
+                    nombre, edad, ID = gestor.obtener_info_paciente()
+                    paciente = Paciente(nombre, edad, ID, gestor.image_3d)
+                    pacientes[ID] = paciente
+                    print(f"El paciente fue registrado: {nombre}, Edad: {edad}, ID: {ID}")
+                except Exception as e:
+                    print(f"Ocurrió un error al crear paciente: {e}")
+            else:
+                print("La clave no se encontró. Primero se debe cargar el DICOM con la opción 'a' del menú.")
 
         
