dc20da287ec221cdfaa015b90cf91955005ac81b
Franches04 <fmariana.salgado@udea.edu.co>
2025-06-14 17:15:54 -0500

    Importar y diccionarios

---
+++ Main.py
@@ -0,0 +1,8 @@
+from Clases import Paciente, DicomHandler, ImagenHandler
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+pacientes = {}
+archivos_dicom = {}
+archivos_imagenes = {}
